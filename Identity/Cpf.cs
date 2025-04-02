f09187653178a9a797d85946432e0c28ddcd8d21
jersonb <jersonlbrito@hotmail.com>
2025-04-01 23:42:55 -0300

    Adicionar arquivos de projeto.

---
+++ Identity/Cpf.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Identity;
+
+public class Cpf
+{
+    public string Value { get; }
+    public string Masked { get; }
+    public bool IsValid { get; }
+
+    public Cpf(string cpf)
+    {
+        cpf = string.Join("", cpf.Where(char.IsNumber));
+        if (cpf.Length != 11)
+        {
+            throw new ArgumentException("CPF deve ter 11 caracteres", nameof(cpf));
+        }
+
+        Value = cpf;
+        Masked = Regex.Replace(cpf, @"(\d{3})(\d{3})(\d{3})(\d{2})", "$1.$2.$3-$4", RegexOptions.Compiled, TimeSpan.FromMicroseconds(2));
+        IsValid = Validate(cpf);
+    }
+
+    private static bool Validate(string cpf)
+    {
+        return Validate(cpf, 9, 0) && Validate(cpf, 10, 1);
+    }
+
+    private static bool Validate(string cpf, int countChar, int digit)
+    {
+        var numbers = cpf[0..countChar];
+        var digitValidator = cpf[9..];
+        var reverseIndex = numbers.Length + 1;
+        var sum = numbers.Aggregate(seed: 0, (atual, next) => atual + int.Parse($"{next}") * reverseIndex--);
+        var mod11 = sum % 11;
+        var mod11result = mod11 < 2 ? 0 : 11 - mod11;
+
+        return digitValidator[digit] == char.Parse($"{mod11result}");
+    }
+
+    public static implicit operator Cpf(string cpf)
+        => new(cpf);
+
+    public static implicit operator string(Cpf cpf)
+        => cpf.Masked;
+
+    public static implicit operator bool(Cpf cpf)
+        => cpf.IsValid;
+}
---
+++ IdentityTests/CpfTests.cs
@@ -0,0 +1,63 @@
+using Identity;
+
+namespace IdentityTests;
+
+public class CpfTests
+{
+    [Theory(DisplayName = "Gerar exceção se não contiver 11 caracteres")]
+    [InlineData("1234567890")]
+    [InlineData("012345678901")]
+    public void Test01(string cpfInput)
+    {
+        Assert.Throws<ArgumentException>(() => new Cpf(cpfInput));
+    }
+
+    [Theory(DisplayName = "Contem apenas números mesmo que seja informado outros caracteres")]
+    [InlineData("123.456.789-00", "12345678900")]
+    [InlineData("012.345.678/90", "01234567890")]
+    public void Test02(string cpfInput, string finalValue)
+    {
+        var value = new Cpf(cpfInput).Value;
+        Assert.Equal(finalValue, value);
+    }
+
+    [Theory(DisplayName = "Valor deve poder ser usado formatado com a máscara '###.###.###-##'")]
+    [InlineData("123.456.789-00", "123.456.789-00")]
+    [InlineData("012.345.678/90", "012.345.678-90")]
+    public void Test03(string cpfInput, string finalValue)
+    {
+        var value = new Cpf(cpfInput).Masked;
+        Assert.Equal(finalValue, value);
+    }
+
+    [Theory(DisplayName = "CPF deve ser válido")]
+    [InlineData("24535170070")]
+    [InlineData("71179874056")]
+    [InlineData("00383021014")]
+    public void Test04(string cpfInput)
+    {
+        var value = new Cpf(cpfInput);
+        Assert.True(value.IsValid);
+    }
+
+    [Theory(DisplayName = "Conversão implícita de string para objeto válido")]
+    [InlineData("24535170070", "245.351.700-70")]
+    [InlineData("71179874056", "711.798.740-56")]
+    [InlineData("00383021014", "003.830.210-14")]
+    public void Test05(string cpfInput, string masked)
+    {
+        Cpf cpf = cpfInput;
+        Assert.True(cpf);
+        Assert.Equal(masked, cpf);
+    }
+
+    [Theory(DisplayName = "Conversão implícita de string para objeto com máscara")]
+    [InlineData("24535170070", "245.351.700-70")]
+    [InlineData("71179874056", "711.798.740-56")]
+    [InlineData("00383021014", "003.830.210-14")]
+    public void Test06(string cpfInput, string masked)
+    {
+        Cpf cpf = cpfInput;
+        Assert.Equal(masked, cpf);
+    }
+}
